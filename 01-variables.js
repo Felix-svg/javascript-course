82c3ea5ab9cdc6a24eee1d21754edcccb14ad642
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-24 00:26:02 +0300

    Implemented code that add values to cart and logs them in the console

---
+++ 01-variables.js
@@ -0,0 +1,48 @@
+let cartQuantity = 0;
+
+function quantity() {
+  const quantityButton = document.getElementById("quantity");
+  quantityButton.addEventListener("click", function () {
+    //let cartQuantity = 0;
+    console.log(`Cart quantity: ${cartQuantity} `);
+    return cartQuantity;
+  });
+}
+quantity();
+
+function add() {
+  const addButton = document.getElementById("add");
+  addButton.addEventListener("click", function () {
+    cartQuantity++;
+    console.log(`Cart quantity: ${cartQuantity}`);
+    return cartQuantity;
+  });
+}
+add();
+
+function two() {
+  const twoButton = document.getElementById("two");
+  twoButton.addEventListener("click", function () {
+    cartQuantity = cartQuantity + 2;
+    console.log(`Cart quantity: ${cartQuantity}`);
+  });
+}
+two();
+
+function three() {
+  const threeButton = document.getElementById("three");
+  threeButton.addEventListener("click", function () {
+    cartQuantity = cartQuantity + 3;
+    console.log(`Cart quantity: ${cartQuantity}`);
+  });
+}
+three();
+
+function reset() {
+  const resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", function () {
+    cartQuantity = 0;
+    console.log(`Cart quantity: ${cartQuantity}`);
+  });
+}
+reset();
