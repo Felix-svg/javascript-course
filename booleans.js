87eb63962d8b1d87d61fa2f48ccee7e9bde40421
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-26 00:32:57 +0300

    implement a simple version of the rock, paper, scissors game

--- booleans.js
+++ booleans.js
@@ -3,14 +3,87 @@ function rockMove() {
   //const randomNumber = Math.random();
   rock.addEventListener("click", function () {
     const randomNumber = Math.random();
+    let computerMove = "";
 
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
-      console.log("rock");
+      computerMove = "rock";
     } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-      console.log("paper");
+      computerMove = "paper";
     } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-      console.log("scissors");
+      computerMove = "scissors";
     }
+
+    console.log(computerMove);
+
+    let result = "";
+    if (computerMove === "rock") {
+      result = "Tie";
+    } else if (computerMove === "paper") {
+      result = "You lose";
+    } else if (computerMove === "scissors") {
+      result = "You win";
+    }
+
+    alert(`You picked rock. Computer picked ${computerMove}. ${result}.`);
   });
 }
+
 rockMove();
+
+function paperMove() {
+  const paper = document.getElementById("paper");
+  paper.addEventListener("click", function () {
+    const randomNumber = Math.random();
+    let computerMove = "";
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = "rock";
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = "paper";
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = "scissors";
+    }
+    console.log(computerMove)
+
+    let result = ''
+    if (computerMove === 'rock'){
+        result = 'You win'
+    }else if(computerMove === 'paper'){
+        result = 'Tie'
+    }else if(computerMove === 'scissors'){
+        result = 'You lose'
+    }
+
+    alert(`You picked paper. Computer picked ${computerMove}. ${result}`)
+  });
+}
+paperMove()
+
+function scissorsMove() {
+    const paper = document.getElementById("scissors");
+    paper.addEventListener("click", function () {
+      const randomNumber = Math.random();
+      let computerMove = "";
+  
+      if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = "rock";
+      } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = "paper";
+      } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = "scissors";
+      }
+      console.log(computerMove)
+  
+      let result = ''
+      if (computerMove === 'rock'){
+          result = 'You lose'
+      }else if(computerMove === 'paper'){
+          result = 'You win'
+      }else if(computerMove === 'scissors'){
+          result = 'Tie'
+      }
+  
+      alert(`You picked scissors. Computer picked ${computerMove}. ${result}`)
+    });
+  }
+  scissorsMove()
