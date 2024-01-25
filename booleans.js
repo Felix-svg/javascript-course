2717b163d134b8c4a14545f380d5544d2a71e529
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-26 01:09:50 +0300

    refractor code

--- booleans.js
+++ booleans.js
@@ -1,19 +1,21 @@
+let computerMove = "";
+
 function rockMove() {
   const rock = document.getElementById("rock");
-  //const randomNumber = Math.random();
   rock.addEventListener("click", () => {
-    const randomNumber = Math.random();
-    let computerMove = "";
+    // const randomNumber = Math.random();
+    // let computerMove = "";
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-      computerMove = "rock";
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-      computerMove = "paper";
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-      computerMove = "scissors";
-    }
+    // if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    //   computerMove = "rock";
+    // } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    //   computerMove = "paper";
+    // } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    //   computerMove = "scissors";
+    // }
 
-    console.log(computerMove);
+    // console.log(computerMove);
+    pickComputerMove()
 
     let result = "";
     if (computerMove === "rock") {
@@ -33,57 +35,74 @@ rockMove();
 function paperMove() {
   const paper = document.getElementById("paper");
   paper.addEventListener("click", () => {
-    const randomNumber = Math.random();
-    let computerMove = "";
+    // const randomNumber = Math.random();
+    // let computerMove = "";
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-      computerMove = "rock";
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-      computerMove = "paper";
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-      computerMove = "scissors";
+    // if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    //   computerMove = "rock";
+    // } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    //   computerMove = "paper";
+    // } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    //   computerMove = "scissors";
+    // }
+    // console.log(computerMove);
+
+    pickComputerMove
+
+    let result = "";
+    if (computerMove === "rock") {
+      result = "You win";
+    } else if (computerMove === "paper") {
+      result = "Tie";
+    } else if (computerMove === "scissors") {
+      result = "You lose";
     }
-    console.log(computerMove)
 
-    let result = ''
-    if (computerMove === 'rock'){
-        result = 'You win'
-    }else if(computerMove === 'paper'){
-        result = 'Tie'
-    }else if(computerMove === 'scissors'){
-        result = 'You lose'
-    }
-
-    alert(`You picked paper. Computer picked ${computerMove}. ${result}`)
+    alert(`You picked paper. Computer picked ${computerMove}. ${result}`);
   });
 }
-paperMove()
+paperMove();
 
 function scissorsMove() {
-    const paper = document.getElementById("scissors");
-    paper.addEventListener("click", () => {
-      const randomNumber = Math.random();
-      let computerMove = "";
-  
-      if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = "rock";
-      } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = "paper";
-      } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = "scissors";
-      }
-      console.log(computerMove)
-  
-      let result = ''
-      if (computerMove === 'rock'){
-          result = 'You lose'
-      }else if(computerMove === 'paper'){
-          result = 'You win'
-      }else if(computerMove === 'scissors'){
-          result = 'Tie'
-      }
-  
-      alert(`You picked scissors. Computer picked ${computerMove}. ${result}`)
-    });
+  const scissors = document.getElementById("scissors");
+  scissors.addEventListener("click", () => {
+    // const randomNumber = Math.random();
+    // let computerMove = "";
+
+    // if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    //   computerMove = "rock";
+    // } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    //   computerMove = "paper";
+    // } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    //   computerMove = "scissors";
+    // }
+    // console.log(computerMove);
+    pickComputerMove()
+
+    let result = "";
+    if (computerMove === "rock") {
+      result = "You lose";
+    } else if (computerMove === "paper") {
+      result = "You win";
+    } else if (computerMove === "scissors") {
+      result = "Tie";
+    }
+
+    alert(`You picked scissors. Computer picked ${computerMove}. ${result}`);
+  });
+}
+scissorsMove();
+
+function pickComputerMove() {
+   const randomNumber = Math.random();
+//   let computerMove = "";
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
   }
-  scissorsMove()
+  console.log(computerMove);
+}
